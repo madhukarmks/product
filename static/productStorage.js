a6339c6786712eb487855a4de3e16cc3b2b1ed46
madhukarmks <madhukarpathraura@gmail.com>
2024-12-29 19:07:54 +0530

    Add files via upload

---
+++ static/productStorage.js
@@ -0,0 +1,30 @@
+
+function productStorage() {
+    
+    return [
+        {type: 'sofas', name: 'Coral Risk', price: 2050.00},
+        {type: 'sofas', name: 'Ocean Bliss', price: 2900.00},
+        {type: 'sofas', name: 'Blue Rays', price: 3408.00},
+        {type: 'sofas', name: 'Love Charm', price: 2750.00},
+        {type: 'sofas', name: 'Green Armour', price: 3100.00},
+        {type: 'sofas', name: "Benjamin's Best", price: 2259.00},
+        {type: 'sofas', name: 'Sea Party', price: 3809.00},
+        {type: 'sofas', name: 'Mamba Jamba', price: 2608.00},
+        {type: 'beds', name: 'Penny Flex', price: 4208.00},
+        {type: 'beds', name: 'Oxford Ovals', price: 8508.00},
+        {type: 'beds', name: 'Sharp Jade', price: 8490.00},
+        {type: 'beds', name: 'Tokyo Town', price: 9505.00},
+        {type: 'beds', name: 'Vast Venny', price: 7530.00},
+        {type: 'beds', name: 'Pink Drip', price: 8485.00},
+        {type: 'beds', name: 'Mysterious Den', price: 6435.00},
+        {type: 'beds', name: 'Forest Fence', price: 9445.00},
+        {type: 'chairs', name: 'Yuki Chooper', price: 7215.00},
+        {type: 'chairs', name: 'Incredible Yolk', price:5240.00},
+        {type: 'chairs', name: 'Blue Whales', price: 6275.00},
+        {type: 'chairs', name: 'Lemon Lint', price: 9230.00},
+        {type: 'chairs', name: 'Workshop Bob', price: 8225.00},
+        {type: 'chairs', name: 'Raining Fire', price: 9255.00},
+        {type: 'chairs', name: 'Wooden Wander', price: 5265.00},
+        {type: 'chairs', name: "Bug's Beauty", price: 7275.00},
+    ]
+}
